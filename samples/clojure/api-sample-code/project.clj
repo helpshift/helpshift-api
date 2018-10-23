fac08f4f7c74705de0569082443b50f5620c4ee8
pardeep <pardeep@helpshift.com>
2018-10-23 12:46:32 +0530

    Update Cheshire version in Clojure Project

--- samples/clojure/api-sample-code/project.clj
+++ samples/clojure/api-sample-code/project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [http-kit "2.2.0"]
                  [com.novemberain/pantomime "2.9.0"]
-                 [cheshire "5.7.1"]
+                 [cheshire "5.8.1"]
                  [http-kit.fake "0.2.1"]]
   :source-path "src"
   :manifest {"Project-Name" ~#(:name %)
